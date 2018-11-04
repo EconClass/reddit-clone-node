666230417d0c242b05263359361f56a1f52ab997
EconClass <bizmail327@gmail.com>
2018-11-04 11:25:58 -0800

    added controller and views

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
 //=================================INITIAL=================================\\
 const express = require('express');
 const app = express();
+const path = require('path');
 const exphbs = require('express-handlebars').create({
     layoutsDir: path.join(__dirname, "views/layouts"),
     partialsDir: path.join(__dirname, "views/partials"),
     defaultLayout: 'main',
     extname: 'hbs'
-});
+  });
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 // const mongoose = require('mongoose');
 
 //=================================MIDDLEWARE=================================\\
+
+// Handlebars
 app.engine('hbs', exphbs.engine)
 app.set('view engine', 'hbs');
-app.use(bodyParser.urlencoded({ extended: true }));
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator()); // Add after body parser initialization!
+
+// Method Override
 app.use(methodOverride('_method'));
 
+//=================================CONTROLLERS=================================\\
+
+// Posts
+require('./controllers/posts.js')(app);
 
 //=================================LISTEN=================================\\
 app.listen(port, () => {
