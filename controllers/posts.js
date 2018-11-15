93caeed589c063c1a050cd82671e17f947044b20
EconClass <bizmail327@gmail.com>
2018-11-14 21:12:09 -0800

    [add] SHOW post route

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,26 +2,33 @@ const Post = require('../models/post.js');
 
 module.exports = (app) => {
     // INDEX
-    app.get('/', (req, res) => {
-        Post.find({})
-            .then(posts => {
-                res.render("posts-index.hbs", { posts });
-            })
-            .catch(err => {
-                console.log(err.message);
-            });
+    app.get('/', function(req, res) {
+        Post.find({}).then(posts => {
+            res.render("posts-index.hbs", { posts });
+        }).catch(err => {
+            console.log(err.message);
+        });
     });
 
     //NEW
-    app.get("/posts/new", (req, res) => {
+    app.get("/posts/new", function(req, res) {
         res.render("posts-new.hbs");
     });
 
     // CREATE
-    app.post('/posts', (req, res) => {
+    app.post('/posts', function(req, res) {
         const post = new Post(req.body);
-        post.save ((err, post) => {
+        post.save ( (err, post) => {
             return res.redirect(`/`);
         })
+    });
+
+    // SHOW one Post
+    app.get("/posts/:id", function(req, res) {
+        Post.findById( req.params.id ).then(  post => {
+            res.render("posts-show.hbs", { post });
+        }).catch(err => {
+            console.log(err.message);
+        });
     });
 };
